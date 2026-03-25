53991a38ab482f98440f464e1c04b3437adbfd9a
Juitem JoonWoo Kim <juitem@gmail.com>
2026-03-25 09:24:37 +0900

    chore: improve clean.sh to remove pycache, pyc files, and build artifacts

--- clean.sh
+++ clean.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 rm -rf ./venv
+find . -type d -name "__pycache__" -exec rm -rf {} + 2>/dev/null
+find . -type f -name "*.pyc" -delete 2>/dev/null
+find . -type f -name "*.pyo" -delete 2>/dev/null
+rm -rf ./.pytest_cache
+rm -rf ./frontend-react/dist
